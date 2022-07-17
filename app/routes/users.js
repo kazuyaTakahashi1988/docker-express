9e008cfc6cc5883dd80db706cf9c815fdb8316dc
kazuyaTakahashi1988 <kazuya.takahashi.631130@gmail.com>
2022-07-17 15:17:57 +0900

    SET

--- app/routes/users.js
+++ app/routes/users.js
@@ -4,10 +4,12 @@ var router = express.Router();
 // sequelizeの設定を追加
 const { Sequelize } = require('sequelize');
 // databaseやuser, passwordをdcoker-compose.ymlで設定したものを使う↓
-const sequelize = new Sequelize('my_mysql_db', 'root', 'root', {
-  host: 'my_mysql', // hostの名前をdocker-compose.ymlで設定したmy_mysqlに変更する
+const sequelize = new Sequelize('express_db', 'root', 'root', {
+  host: 'express-mysql', // hostの名前をdocker-compose.ymlで設定したmy_mysqlに変更する
   dialect: 'mysql',
 });
+
+let word;
 
 /* GET users listing. */
 router.get('/', async function (req, res, next) {
@@ -15,11 +17,14 @@ router.get('/', async function (req, res, next) {
   // my_mysqlに接続されているかテスト
   try {
     await sequelize.authenticate();
+    word = "success";
     console.log('Connection has been established successfully.');
+
   } catch (error) {
+    word = "error";
     console.error('Unable to connect to the database:', error);
   }
-  res.send('respond with a resource');
+  res.send('respond with a resource ' + word);
 });
 
 module.exports = router;
