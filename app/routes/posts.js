5592d72893c4f763d486efbce071455ee2a7a49c
kazuyaTakahashi1988 <kazuya.takahashi.631130@gmail.com>
2022-07-27 22:54:56 +0900

    SET

--- app/routes/posts.js
+++ app/routes/posts.js
@@ -5,84 +5,88 @@ const Post = require('./../models').Post;
 const User = require('./../models').User;
 const Category = require('./../models').Category;
 
+const perPage = 6; // 表示ページ数
+
 /* --------------------------------------
     ▽ 記事一覧 ▽
 -------------------------------------- */
 
-/* GET 記事 一覧 */
+/* Q & A 一覧 */
 router.get('/', async (req, res, next) => {
-    const page = req.query.page || 1;
-    const perPage = 6;
+    const page = req.query.page || 1; // ?page= のクエリ情報
+    
     Post.findAndCountAll({
         order: [['id', 'DESC']],
         offset: (page - 1) * perPage,
         limit: perPage
     }).then(result => {
         const posts = result.rows; // 取得記事
-        const count = Math.floor(result.count / perPage) + 1; // ページ数
+        const count = Math.floor(result.count / perPage) + 1; // ページ総数
         res.render('posts', {
-            user: req.user,
-            posts,
-            count,
-            page,
-            pegePath: '',
-            pageName: ''
+            user: req.user, // 認証ユーザー情報
+            posts, // 取得記事情報
+            count, // ページ総数（ページャー生成用）
+            page, // ページ総数から何件目（ページャー生成用）
+            pegePath: '', // ページのパス（ページャー生成用）
+            pageTitle: 'Q & A 一覧' // ページのタイトル
         });
     });
 });
 
-/* GET ユーザー記事 一覧 */
+/* ユーザー記事 一覧 */
 router.get('/user/:id', async (req, res, next) => {
     const page = req.query.page || 1;
-    const perPage = 6;
-    let pageName;
+    
+    let pageTitle;
     User.findOne({ where: { id: req.params["id"] } }
     ).then(userOne => {
-        pageName = userOne.name
-    });
+        pageTitle = userOne.name
+    }); // ページのタイトル取得
+
     Post.findAndCountAll({
         order: [['id', 'DESC']],
         where: { user_id: req.params["id"] },
         offset: (page - 1) * perPage,
         limit: perPage
     }).then(result => {
-        const posts = result.rows; // 取得記事
-        const count = Math.floor(result.count / perPage) + 1; // ページ数
+        const posts = result.rows;
+        const count = Math.floor(result.count / perPage) + 1;
         res.render('posts', {
             user: req.user,
             posts,
             count,
             page,
             pegePath: `/user/${req.params["id"]}`,
-            pageName
+            pageTitle: `ユーザー：${pageTitle}`
         });
     });
 });
 
-/* GET カテゴリー記事 一覧 */
+/* カテゴリー記事 一覧 */
 router.get('/category/:id', async (req, res, next) => {
     const page = req.query.page || 1;
-    const perPage = 6;
-    let pageName;
+
+    let pageTitle;
     Category.findOne({ where: { id: req.params["id"] } }
     ).then(categoryOne => {
-        pageName = categoryOne.category_name
-    });
+        pageTitle = categoryOne.category_name
+    }); // ページのタイトル取得
+
     Post.findAndCountAll({
         order: [['id', 'DESC']],
         where: { category_id: req.params["id"] },
         offset: (page - 1) * perPage,
         limit: perPage
     }).then(result => {
-        const posts = result.rows; // 取得記事
-        const count = Math.floor(result.count / perPage) + 1; // ページ数
+        const posts = result.rows;
+        const count = Math.floor(result.count / perPage) + 1;
         res.render('posts', {
             user: req.user,
             posts,
             count,
             page,
             pegePath: `/category/${req.params["id"]}`,
-            pageName
+            pageTitle: `カテゴリー：${pageTitle}`
         });
     });
 });
@@ -91,9 +95,8 @@ router.get('/category/:id', async (req, res, next) => {
     ▽ 記事詳細 ▽
 -------------------------------------- */
 
-/* GET 記事 詳細 */
+/* 記事 詳細 */
 router.get('/detail/:id', async (req, res, next) => {
-    const user = req.user;
     Post.findOne({
         where: { id: req.params["id"] },
         include: [{ model: User }, { model: Category }]
