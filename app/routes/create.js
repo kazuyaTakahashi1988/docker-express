50df58ed8940571249e05a4ff3182beaa4cf6c25
kazuyaTakahashi1988 <kazuya.takahashi.631130@gmail.com>
2022-07-28 23:25:52 +0900

    SET

--- app/routes/create.js
+++ app/routes/create.js
@@ -39,26 +39,26 @@ router.get('/post', async (req, res, next) => {
 /* POST 作成処理 */
 router.post('/post', upload.single('image'), async (req, res, next) => {
 
-    /* ▽ 画像ネーミング用　日付取得処理 ▽  */
-    var nowDate = new Date();
-    var Now = String(nowDate.getFullYear()) + String(nowDate.getMonth() + 1) + String(nowDate.getDate()) + String(nowDate.getHours()) + String(nowDate.getMinutes()) + String(nowDate.getSeconds());
+    /* ▽ 保存画像ネーミング ▽  */
+    const nowDate = new Date();
+    let saveImageName = "PostImage-".concat(nowDate.getFullYear(), nowDate.getMonth(), nowDate.getDate(), nowDate.getHours(), nowDate.getMinutes(), nowDate.getSeconds(), Math.random().toString(36).slice(-10), '.jpg');
 
     /* ▽ 画像圧縮 処理 ▽  */
-    let image;
     if (req.file) {
         sharp(destDir + req.file.originalname)
             .resize(340)
             .toFormat("jpg")
-            .jpeg({ quality: 30 })
-            .toFile(destDir + Now + req.file.originalname, () => {
+            .jpeg({ quality: 75 }) // 圧縮率 0〜100
+            .toFile(destDir + saveImageName, () => {
                 fs.unlinkSync(destDir + req.file.originalname); // 元の画像を削除
             });
-        image = Now + req.file.originalname;
-    } else { image = ''; }
+    } else {
+        saveImageName = '';
+    }
 
     /* ▽ 記事クリエイト 処理 ▽  */
     Post.create({
-        image: image,
+        image: saveImageName,
         title: req.body.title,
         content: req.body.content,
         user_id: req.user.id,
