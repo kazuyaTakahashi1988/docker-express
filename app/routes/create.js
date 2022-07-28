1cde2d0759ce03e670fd40a254e1512f8d385982
kazuyaTakahashi1988 <kazuya.takahashi.631130@gmail.com>
2022-07-28 15:31:58 +0900

    SET

--- app/routes/create.js
+++ app/routes/create.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const sharp = require('sharp');
 
 const Post = require('./../models').Post;
 const User = require('./../models').User;
 const Category = require('./../models').Category;
 
+const destDir = 'public/uploads/';
+const storage = multer.diskStorage({
+    destination: destDir,
+    filename: function (req, file, cb) {
+        cb(null, file.originalname)
+    }
+})
+
+const upload = multer({
+    storage: storage
+});
 /* --------------------------------------
     ▽ Q & A 記事作成 ▽
 -------------------------------------- */
@@ -21,17 +34,30 @@ router.get('/post', async (req, res, next) => {
     });
 });
 /* POST 作成処理 */
-router.post('/post', async (req, res, next) => {
-    // res.send(req.body);
-    Post.create({
-        title: req.body.title,
-        content: req.body.content,
-        user_id: req.user.id,
-        category_id: req.body.category_id,
-    }
-    ).then(result => {
-        res.redirect(302, `/posts/detail/${result.id}`);
-    });
+router.post('/post', upload.single('image'), async (req, res, next) => {
+    if (req.file) {
+        res.json(req.file);
+        sharp(req.file.originalname)
+            .resize(64)
+            .toFile(destDir + 'resized' + req.file.originalname, (err, info) => {
+                if (err) {
+                    throw err;
+                }
+                console.log(info);
+            });
+        console.log("file: ", req.file);
+        res.send(req.body);
+    } else throw 'error';
+    res.send(req.body);
+    // Post.create({
+    //     title: req.body.title,
+    //     content: req.body.content,
+    //     user_id: req.user.id,
+    //     category_id: req.body.category_id,
+    // }
+    // ).then(result => {
+    //     res.redirect(302, `/posts/detail/${result.id}`);
+    // });
 });
 
 module.exports = router;
