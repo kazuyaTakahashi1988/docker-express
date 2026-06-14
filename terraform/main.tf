1eb4a63b47296040b1fb704aeaebf11d196c1c18
kazuyaTakahashi1988 <kazuya.takahashi.631130@gmail.com>
2026-06-14 22:52:38 +0900

    修正

--- terraform/main.tf
+++ terraform/main.tf
@@ -269,8 +269,9 @@ resource "google_cloud_run_v2_service" "app" {
 }
 
 resource "google_cloud_run_v2_service_iam_member" "public_invoker" {
+  project  = var.project_id
   location = google_cloud_run_v2_service.app.location
-  service  = google_cloud_run_v2_service.app.name
+  name  = google_cloud_run_v2_service.app.name
   role     = "roles/run.invoker"
   member   = "allUsers"
 }
